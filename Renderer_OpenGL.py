2de142d6883788df04633c025d79e81dfd8f99b6
andresdlRoca <andresdelaroca11@gmail.com>
2022-11-09 16:29:37 -0600

    5 models set

--- Renderer_OpenGL.py
+++ Renderer_OpenGL.py
@@ -52,21 +52,21 @@ while isRunning:
             elif event.key == pygame.K_x:
                 rend.wireframeMode()
             
-            elif event.key == pygame.K_1: #Shader default
-                rend.setShaders(vertex_shader, fragment_shader)
+            # elif event.key == pygame.K_1: #Shader default
+            #     rend.setShaders(vertex_shader, fragment_shader)
             
-            elif event.key == pygame.K_2: #Toon Shader
-                rend.setShaders(vertex_shader, toon_shader)
+            # elif event.key == pygame.K_2: #Toon Shader
+            #     rend.setShaders(vertex_shader, toon_shader)
             
-            elif event.key == pygame.K_3: #Glow Shader
-                rend.setShaders(vertex_shader, glow_shader)
+            # elif event.key == pygame.K_3: #Glow Shader
+            #     rend.setShaders(vertex_shader, glow_shader)
             
-            elif event.key == pygame.K_4: #Pink Jelly
-                rend.setShaders(vertex_shader, pinkJelly_shader)
-            elif event.key == pygame.K_5: #Pulsating shader
-                rend.setShaders(pulse_vertex_shader, pulse_fragment_shader)
+            # elif event.key == pygame.K_4: #Pink Jelly
+            #     rend.setShaders(vertex_shader, pinkJelly_shader)
+            # elif event.key == pygame.K_5: #Pulsating shader
+            #     rend.setShaders(pulse_vertex_shader, pulse_fragment_shader)
 
-
+    #General camera controls
     if keys[K_a]:
         if vertCounter > 0:
             rend.camPosition.x -= 10 * deltaTime
@@ -110,6 +110,7 @@ while isRunning:
         else:
             print("Limite de zoom out alcanzado")
 
+    #Light control
     if keys[K_LEFT]:
         rend.pointLight.x -= 10 * deltaTime
 
@@ -121,6 +122,89 @@ while isRunning:
     
     elif keys[K_DOWN]:
         rend.pointLight.y -= 10 * deltaTime
+    
+    #Shaders
+    if keys[K_1]: #Shader default
+        rend.setShaders(vertex_shader, fragment_shader)
+
+    elif keys[K_2]: #Toon shader
+        rend.setShaders(vertex_shader, toon_shader)
+
+    elif keys[K_3]: #Glow/negative shader
+        rend.setShaders(vertex_shader, glow_shader)
+
+    elif keys[K_4]: #Pink jelly shader
+        rend.setShaders(vertex_shader, pinkJelly_shader)
+    
+    elif keys[K_5]: #Pulse shader
+        rend.setShaders(pulse_vertex_shader, pulse_fragment_shader)
+
+
+    if keys[K_LSHIFT] and keys[K_1]:
+        rend.scene.clear()
+        face = Model("seraphine.obj", "seraphine.bmp")
+
+        face.position.z -= 5
+        face.scale.x = 2
+        face.scale.y = 2
+        face.scale.z = 2
+
+        rend.scene.append( face )
+        print("Modelo 1")
+    
+    elif keys[K_LSHIFT] and keys[K_2]:
+        rend.scene.clear()
+        face = Model("fallguy.obj", "seraphine.bmp")
+
+        face.position.z -= 5
+        face.position.y -= 1
+        face.scale.x = 2
+        face.scale.y = 2
+        face.scale.z = 2
+        rend.scene.clear()
+        rend.scene.append( face )
+        print("Modelo 2")
+
+    elif keys[K_LSHIFT] and keys[K_3]:
+        rend.scene.clear()
+        # rend.target.z = -20
+        face = Model("girl.obj", "girl.bmp")
+        face.position.y -= 5
+        face.position.z -= 5
+        face.scale.x = 0.05
+        face.scale.y = 0.05
+        face.scale.z = 0.05
+
+        rend.scene.append( face )
+        print("Modelo 3")
+
+    elif keys[K_LSHIFT] and keys[K_4]:
+        rend.scene.clear()
+
+        face = Model("mario.obj", "bone.bmp")
+
+        face.position.z -= 5
+        face.position.y -= 5
+        face.scale.x = 1
+        face.scale.y = 1
+        face.scale.z = 1
+
+        rend.scene.append( face )
+        print("Modelo 4")
+
+    elif keys[K_LSHIFT] and keys[K_5]:
+        rend.scene.clear()
+
+        face = Model("stormtrooper.obj", "stormtrooper.bmp")
+
+        face.position.z -= 5
+        face.position.y -= 3
+        face.scale.x = 2
+        face.scale.y = 2
+        face.scale.z = 2
+
+        rend.scene.append( face )
+        print("Modelo 5")
 
     deltaTime = clock.tick(60) / 1000
     rend.time += deltaTime
